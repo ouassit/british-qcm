fbf080c27badd5dad9d74a02052739997f2bb189
ouassit <ouassit.youssef@gmail.com>
2023-05-08 12:51:21 +0100

    a

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             //'cors' => \App\Http\Middleware\Cors::class, 
-            'throttle:api',
+            //'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
