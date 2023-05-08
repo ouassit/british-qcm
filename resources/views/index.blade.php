70e6d23fdf0261d3b0d00f2aadc0d216a427711e
ouassit <ouassit.youssef@gmail.com>
2023-05-08 08:02:48 +0100

    update 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <title>Your App Title Here</title>
     <base href="/">
+    <link rel="stylesheet" src="{{ asset('client/styles.css') }}" />
 </head>
 <body>
     <app-root></app-root>
     <script src="{{ asset('client/runtime.js') }}"></script>
     <script src="{{ asset('client/polyfills.js') }}"></script>
-    <script src="{{ asset('client/styles.js') }}"></script>
     <script src="{{ asset('client/vendor.js') }}"></script>
     <script src="{{ asset('client/main.js') }}"></script>
 </body>
