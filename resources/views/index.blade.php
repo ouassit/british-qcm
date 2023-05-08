20660202bc9cad2db7b6227ac758567ac45d8420
ouassit <ouassit.youssef@gmail.com>
2023-05-08 07:56:38 +0100

    update 2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your App Title Here</title>
+    <base href="/">
+</head>
+<body>
+    <app-root></app-root>
+    <script src="{{ asset('js/client/runtime.js') }}"></script>
+    <script src="{{ asset('js/client/polyfills.js') }}"></script>
+    <script src="{{ asset('js/client/styles.js') }}"></script>
+    <script src="{{ asset('js/client/vendor.js') }}"></script>
+    <script src="{{ asset('js/client/main.js') }}"></script>
+</body>
+</html>
