ebfe061eb2b33e541a8587d4497eef4d7279fbe4
ouassit <ouassit.youssef@gmail.com>
2023-05-08 09:17:47 +0100

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>British Workshop Test</title>
-    <base href="/">
-    <link rel="stylesheet" src="{{ asset('client/styles.css') }}" />
+    <meta http-equiv="refresh" content="0; url=/dashboard">
+
 </head>
 <body>
-    <app-root></app-root>
-    <script src="{{ asset('client/runtime.js') }}"></script>
-    <script src="{{ asset('client/polyfills.js') }}"></script>
-    <script src="{{ asset('client/vendor.js') }}"></script>
-    <script src="{{ asset('client/main.js') }}"></script>
 </body>
 </html>
